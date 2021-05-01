0491b8e73edbb91a3d6a52fe5ea779f54f336453
John Ellison <37640782+johnellison919@users.noreply.github.com>
2021-05-01 03:44:07 -0500

    Updates Homepage Styling
    Adds a sidebar and content section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,28 @@
 		<meta name="description" content="Are you looking for a hard-working, self-motivated website developer? If so, you've come to the right place!">
 	</head>
 	<body>
-		<div class="test">
-			Test
-		</div>
+		<main>
+			@include('partials.aside')
+			<section>
+				<div class="content">
+					<article>
+						<div class="pe-3 my-auto">
+							<picture>
+								<source srcset="PLACEHOLDER.webp" type="image/webp">
+								<img src="PLACEHOLDER.png" alt="PLACEHOLDER" class="PLACEHOLDER">
+							</picture>
+						</div>
+						<div class="my-auto">
+							<h2>
+								<a href="PLACEHOLDER">PLACEHOLDER</a>
+							</h2>
+							<p>
+								PLACEHOLDER CONTENT
+							</p>
+						</div>
+					</article>
+				</div>
+			</section>
+		</main>
 	</body>
 </html>
---
+++ resources/views/partials/aside.blade.php
@@ -0,0 +1,46 @@
+<aside>
+	<div>
+		<h1>John Ellison</h1>
+		<p>
+			Web Developer
+		</p>
+		<ul>
+			<li>
+				<div class="me-2">
+					<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-building" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694L1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+						<path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+					</svg>
+				</div>
+				<div>
+					<span class="sidebar-subtext">Currently Employed At:</span>
+					Footbridge Media, LLC
+				</div>
+			</li>
+			<li>
+				<div class="me-2">
+					<svg width="2rem" height="2rem" viewBox="0 0 16 16" class="bi bi-geo-alt-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+					</svg>
+				</div>
+				<div>
+					<span class="sidebar-subtext">Currently Living In:</span>
+					Florida, USA
+				</div>
+			</li>
+		</ul>
+		<div class="text-center">
+			<a href="https://github.com/johnellison919" target="_blank" rel="noopener">
+				Github
+			</a>
+			|
+			<a href="https://www.linkedin.com/in/johnellison919/" target="_blank" rel="noopener">
+				LinkedIn
+			</a>
+			|
+			<a href="mailto:johnellison919@gmail.com">
+				Email
+			</a>
+		</div>
+	</div>
+</aside>
