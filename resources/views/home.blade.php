d97c852b6433e3705654b73c5aab43c9cf6f16a8
John Ellison <37640782+johnellison919@users.noreply.github.com>
2021-05-01 02:23:29 -0500

    Adjustments to Default Homepage Styling
    This updates the default homepage and updates stylesheets

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link href="/favicon.ico" rel="icon" type="image/x-icon">
+		<link href="{{ asset('style/theme.css') }}" rel="stylesheet">
+
+		<title>Web Developer | John Ellison</title>
+		<meta name="description" content="Are you looking for a hard-working, self-motivated website developer? If so, you've come to the right place!">
+	</head>
+	<body>
+		<div class="test">
+			Test
+		</div>
+	</body>
+</html>
