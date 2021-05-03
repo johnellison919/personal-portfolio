e1bde6cea565abe1d55cbb11aa17b022f789d032
John Ellison <37640782+johnellison919@users.noreply.github.com>
2021-05-03 02:36:50 -0500

    Adds Unique Setting to Slug in Projects Database
    Adds the unique setting to the slug in the projects database to ensure we don't have two pages with the same slug

--- database/migrations/2021_05_02_092551_create_projects_table.php
+++ database/migrations/2021_05_02_092551_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
 			$table->id();
-			$table->string("slug");
+			$table->string("slug")->unique();
 			$table->string("title");
 			$table->text("head");
 			$table->text("body");
